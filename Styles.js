d940fa1b525a3ae31c50991fb03c745fc110810c
Ifthikara <84259096+Ifthikara@users.noreply.github.com>
2021-05-30 12:52:01 +0530

    Add files via upload

---
+++ Styles.js
@@ -0,0 +1,14 @@
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
